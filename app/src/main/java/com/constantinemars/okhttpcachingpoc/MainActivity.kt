4c597f69c41b9aef89f25ceba72b5257ec4d6b2e
Constantine Mars <constantine.mars@gmail.com>
2020-05-22 18:09:37 -0700

    UI updated to trigger multiple requests

--- app/src/main/java/com/constantinemars/okhttpcachingpoc/MainActivity.kt
+++ app/src/main/java/com/constantinemars/okhttpcachingpoc/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +18,17 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
 
         val apiService = ApiClient.retrofit.create(ApiService::class.java)
-        Timber.d("calling \"get\"")
-        compositeDisposable.add(apiService.get()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-            {response -> Timber.d(response.toString())},
-            {error -> Timber.e(error)}
-        ))
+
+        button.setOnClickListener {
+            Timber.d("calling \"get\"")
+            compositeDisposable.add(apiService.get()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {response -> Timber.d(response.toString())},
+                    {error -> Timber.e(error)}
+                ))
+        }
     }
 
     override fun onDestroy() {
