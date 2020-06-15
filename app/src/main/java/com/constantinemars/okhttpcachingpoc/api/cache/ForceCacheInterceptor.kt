42f775e9cda5202ec039757d5b64a2ac273591b9
Constantine Mars <constantine.mars@gmail.com>
2020-06-15 10:41:36 -0700

    Experiments on POST caching (not working yet)

--- app/src/main/java/com/constantinemars/okhttpcachingpoc/api/cache/ForceCacheInterceptor.kt
+++ app/src/main/java/com/constantinemars/okhttpcachingpoc/api/cache/ForceCacheInterceptor.kt
@@ -1,13 +1,44 @@
 package com.constantinemars.okhttpcachingpoc.api.cache
 
-import okhttp3.CacheControl
-import okhttp3.Interceptor
-import okhttp3.Response
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.Buffer
+import okio.BufferedSource
+import java.io.ByteArrayInputStream
 import java.util.concurrent.TimeUnit
+
+class FakeResponseBody(val data: String): ResponseBody() {
+    override fun contentLength(): Long {
+        return data.length.toLong()
+    }
+
+    override fun contentType(): MediaType? {
+        return "text/plain".toMediaType()
+    }
+
+    override fun source(): BufferedSource {
+        val buffer = Buffer()
+        buffer.inputStream().read(data.toByteArray())
+        return buffer
+    }
+
+}
 
 class ForceCacheInterceptor: BaseCacheInterceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val builder = chain.request().newBuilder()
+        val request = chain.request()
+        if("POST" == request.method.toUpperCase()) {
+
+            val body = FakeResponseBody("test data")
+            val response = Response.Builder()
+                .body(body)
+                .build()
+
+            return response
+        }
+
+        val builder = request.newBuilder()
             .cacheControl(CacheControl.Builder()
                 .onlyIfCached()
                 .maxStale(30, TimeUnit.SECONDS) // same as CacheControl.FORCE_CACHE but with max-stale limited to 1 day
